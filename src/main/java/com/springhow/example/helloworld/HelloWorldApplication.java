085f73075c120c3772c3da980f13779b8ce08977
Sandeep Gill <104765084+MrGill88@users.noreply.github.com>
2023-06-23 10:57:16 +0530

    Update HelloWorldApplication.java 23 6 23
    new chnages on code

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Welcome to CI-CD Pipeline Demo automation by Sandeep Gill code change on 17-05-2023..";
+        return "Welcome to CI-CD Pipeline Demo automation by Sandeep Gill code change on 23-06-2023..";
     }
 
 }
